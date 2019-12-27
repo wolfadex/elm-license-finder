f2146106385a0db7ff0694124200846eaa61cf13
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2019-12-27 21:50:11 +0900

    run prettier

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const os = require("os");
 
-const elmHome = process.env.ELM_HOME || path.resolve(os.homedir(), ".elm/0.19.1/packages");
+const elmHome =
+  process.env.ELM_HOME || path.resolve(os.homedir(), ".elm/0.19.1/packages");
 const dependencyDict = {};
 const dependenciesToGet = {};
 const initialElmJson = fs.readFileSync(path.resolve(process.cwd(), "elm.json"));
@@ -19,42 +20,51 @@ Object.entries(indirect).forEach(function([package, version]) {
 });
 
 while (Object.keys(dependenciesToGet).length > 0) {
-	const [ package, version ] = Object.entries(dependenciesToGet)[0];
-	const [ who, what ] = package.split("/");
-	const { license, dependencies, actualVersion } = getLicense(who, what, version);
+  const [package, version] = Object.entries(dependenciesToGet)[0];
+  const [who, what] = package.split("/");
+  const { license, dependencies, actualVersion } = getLicense(
+    who,
+    what,
+    version,
+  );
 
-	if (dependencyDict[`${package}/${actualVersion}`] == null) {
-	  dependencyDict[`${package}/${actualVersion}`] = license;
-	  delete dependenciesToGet[package];
-	} else {
-	Object
-	  Object.entries(dependencies).forEach(function([pack, ver]) {
-	    dependenciesToGet[pack] = ver;
-	  });
-	}
+  if (dependencyDict[`${package}/${actualVersion}`] == null) {
+    dependencyDict[`${package}/${actualVersion}`] = license;
+    delete dependenciesToGet[package];
+  } else {
+    Object;
+    Object.entries(dependencies).forEach(function([pack, ver]) {
+      dependenciesToGet[pack] = ver;
+    });
+  }
 }
-
 
 function getLicense(who, what, version) {
   let elmJson;
   let actualVersion;
 
   if (isVersion(version)) {
-  	// Exact version
-  	elmJson = fs.readFileSync(path.join(elmHome, who, what, version, "elm.json"));
-  	actualVersion = version;
+    // Exact version
+    elmJson = fs.readFileSync(
+      path.join(elmHome, who, what, version, "elm.json"),
+    );
+    actualVersion = version;
   } else {
-  	// Between 2 versions
-  	const possibleVersions = fs.readdirSync(path.join(elmHome, who, what), { withFileTypes: true });
-  	const highestVersion = possibleVersions
-  	  .filter(function(fileOrDir) {
-	  	return fileOrDir.isDirectory && isVersion(fileOrDir.name);
-	  })
-	  .map(function(dir) {
-	  	return dir.name;
-	  });
-	elmJson = fs.readFileSync(path.join(elmHome, who, what, highestVersion, "elm.json"));
-	actualVersion = highestVersion;
+    // Between 2 versions
+    const possibleVersions = fs.readdirSync(path.join(elmHome, who, what), {
+      withFileTypes: true,
+    });
+    const highestVersion = possibleVersions
+      .filter(function(fileOrDir) {
+        return fileOrDir.isDirectory && isVersion(fileOrDir.name);
+      })
+      .map(function(dir) {
+        return dir.name;
+      });
+    elmJson = fs.readFileSync(
+      path.join(elmHome, who, what, highestVersion, "elm.json"),
+    );
+    actualVersion = highestVersion;
   }
 
   const { license, dependencies } = JSON.parse(elmJson);
@@ -68,37 +78,37 @@ function isVersion(str) {
 
 function highestVersion(possibleVersions) {
   return possibleVersions
-	.map(function(version) {
-	  return version.split(".");
-	})
-	.sort(function([majA, minA, patA], [majB, minB, patB]) {
-	  if (majA > majB) {
-	  	return 1;
-	  } else if (majA < majB) {
-	  	return -1;
-	  } else if (minA > minB) {
-	  	return 1;
-	  } else if (minA < minB) {
-	  	return -1;
-	  } else if (patA > patB) {
-	  	return 1;
-	  } else if (patA < patB) {
-	  	return -1;
-	  } else {
-	  	return 0;
-	  }
-	})[0];
+    .map(function(version) {
+      return version.split(".");
+    })
+    .sort(function([majA, minA, patA], [majB, minB, patB]) {
+      if (majA > majB) {
+        return 1;
+      } else if (majA < majB) {
+        return -1;
+      } else if (minA > minB) {
+        return 1;
+      } else if (minA < minB) {
+        return -1;
+      } else if (patA > patB) {
+        return 1;
+      } else if (patA < patB) {
+        return -1;
+      } else {
+        return 0;
+      }
+    })[0];
 }
 
 console.log("Dependencies:");
 Object.entries(dependencyDict).forEach(function([pack, license]) {
-  const [ who, what, version ] = pack.split("/");
+  const [who, what, version] = pack.split("/");
   const packageName = `${who}/${what}`;
   let isDirect = false;
 
   if (direct[packageName] === version) {
-  	isDirect = true;
+    isDirect = true;
   }
 
   console.log(isDirect ? "Direct" : "Indirect", packageName, version, license);
-})
+});
